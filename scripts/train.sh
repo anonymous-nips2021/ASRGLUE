80edd7900ad0bcbc2b98c73eec16f6bca401d90f
yun97 <1035677207@qq.com>
2021-09-01 12:56:31 +0800

    Create train.sh

---
+++ scripts/train.sh
@@ -0,0 +1,1 @@
+python run.py --data_dir $DATA_DIR/ --bert_model $BERT_BASE --task_name $TASK --output_dir $OUTPUT/ --num_train_epochs $EPOCH_NUM --test_file $TEST_FILE --dev_file $DEV_FILE --train_file $TRAIN_FILE

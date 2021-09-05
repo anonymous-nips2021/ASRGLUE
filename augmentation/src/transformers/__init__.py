5353f5341eaa9c51e6b50667d892d40784a76e49
yun97 <1035677207@qq.com>
2021-09-05 22:30:10 +0800

    Create __init__.py

---
+++ augmentation/src/transformers/__init__.py
@@ -0,0 +1,6 @@
+from .tokenization_gpt2 import GPT2Tokenizer
+from .configuration_gpt2 import GPT2Config, GPT2_PRETRAINED_CONFIG_ARCHIVE_MAP
+from .modeling_gpt2 import (GPT2PreTrainedModel, GPT2Model,
+                                GPT2LMHeadModel, GPT2DoubleHeadsModel,
+                                #load_tf_weights_in_gpt2, 
+                                GPT2_PRETRAINED_MODEL_ARCHIVE_MAP)
